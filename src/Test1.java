54fbbf8597e28168335b8738f48ac9a760843e4a
kamalbatraone <kamalbatraone@gmail.com>
2025-02-09 13:48:28 +0530

    my first commit

---
+++ src/Test1.java
@@ -0,0 +1,9 @@
+public class Test1 {
+    public static void main(String args[]){
+        for(int i=1; i<11;i++) {
+            System.out.printf("9 X %d = %d%n", i, 9 * i);
+
+        }
+
+    }
+}
